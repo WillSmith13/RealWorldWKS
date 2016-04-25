b6af71f25f830ad97dd09d6189bcd50076cc058e
wills <wills@localhost>
2016-04-25 14:50:35 -0400

    First Code

---
+++ RealWorldWKS/src/edu/saintjoe/cs/wills/RealWorldWKS/Section.java
@@ -0,0 +1,9 @@
+package edu.saintjoe.cs.wills.RealWorldWKS;
+
+public class Section {
+	String room;
+	String time;
+	String title;
+	Student[] roster;
+
+}
---
+++ RealWorldWKS/src/edu/saintjoe/cs/wills/RealWorldWKS/Student.java
@@ -0,0 +1,15 @@
+package edu.saintjoe.cs.wills.RealWorldWKS;
+
+public class Student {
+	String name;
+	int age;
+	String gender;
+	int classYr;
+
+	
+	
+	
+	
+	
+
+}
