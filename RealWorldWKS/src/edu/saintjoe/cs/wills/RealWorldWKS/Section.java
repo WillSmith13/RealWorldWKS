8b0ec51c3bf0378474a7d53456dcc44320453f8a
wills <wills@localhost>
2016-04-29 12:31:34 -0400

    Driver Class

--- RealWorldWKS/src/edu/saintjoe/cs/wills/RealWorldWKS/Section.java
+++ RealWorldWKS/src/edu/saintjoe/cs/wills/RealWorldWKS/Section.java
@@ -1,9 +1,37 @@
 package edu.saintjoe.cs.wills.RealWorldWKS;
 
 public class Section {
-	String room;
-	String time;
-	String title;
-	Student[] roster;
 
+	    public static final int MAXSTUDENTS = 25;
+		
+		public static String[] classYrNames = {"ImpossibleClassYear", "Freshman", "Sophomore", "Junior", "Senior", "Special"};
+		
+		String room;
+		String teacher;
+		String title;
+		Student[] students;
+		
+		public Section (String newTitle, String newTeacher, String newRoom) {
+			students = new Student[MAXSTUDENTS];
+			title = newTitle;
+			teacher = newTeacher;
+			room = newRoom;
+		}
+		
+		
+		public String toString() {
+			
+			String output = "";
+			output = "Class information for " + title + " taught by: " + teacher + " meeting in Room: " + room + "\n";
+			
+			for (Student oneStudent: students) {
+				if (oneStudent == null)
+					break;
+				output += oneStudent.getName() + " " + oneStudent.setGender() + " " + classYrNames[oneStudent.getClassYr()] + " " + oneStudent.getAge() + "\n";
+				}
+			return output;
+		}
+	
+	
+	
 }
